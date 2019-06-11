26971723187a61083ed20aa2dfebc31ff890f86d
mikel <mlygas@gmail.com>
2019-06-11 06:43:04 -0400

    updated table data

--- app_ml.py
+++ app_ml.py
@@ -1,34 +1,56 @@
 import json
+import sqlite3
 
-from flask import Flask
+from flask import Flask, jsonify
 from flask import request
 from flask import make_response
 app = Flask(__name__)
-    
+
+# Create database
+conn = sqlite3.connect('nj_db.db')
+
+
+# Create data files
+rail_file = open('rail.geojson')
+rail_json = json.load(rail_file)
+
+hospital_file = open('Hospitals.geojson')
+hospital_json = json.load(hospital_file)
+
+school_file = open('school.geojson')
+school_json = json.load(school_file)
+
+# Create routes
 @app.route('/hospital_data')
 def hospital_data():
-    hospital_file = open('Hospitals.geojson')
-    hospital_json = json.load(hospital_file)
-
     r = make_response(json.dumps(hospital_json))
     r.headers['Content-Type'] = 'application/json'
     return r
 
 @app.route('/rail_data')
 def rail_data():
-    rail_file = open('rail.geojson')
-    rail_json = json.load(rail_file)
     r = make_response(json.dumps(rail_json))
     r.headers['Content-Type'] = 'application/json'
     return r
 
 @app.route('/school_data')
 def school_data():
-    school_file = open('school.geojson')
-    school_json = json.load(school_file)
+    c = conn.cursor()
     r = make_response(json.dumps(school_json))
     r.headers['Content-Type'] = 'application/json'
     return r
 
+@app.route('/info')
+def info():
+    city = request.args.get('city')
+    county = request.args.get('county')
+    conn = sqlite3.connect('nj_db.db')
+    c = conn.cursor()
+    data = c.execute('SELECT * FROM school WHERE CITY = ? OR COUNTY = ?',[city,county]).fetchall()
+    conn.commit()
+    conn.close()
+    return jsonify(data)
+
+
 if __name__ == '__main__':
-    app.run(debug=False, port=8000, host='0.0.0.0', threaded=True)
+    app.run(debug=False, port=8000, host='localhost', threaded=True)
