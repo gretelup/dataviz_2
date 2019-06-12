51d38cfb0375e2efb0c2f6b7c63da8a8ddc3caee
Gretel <gretelup@gmail.com>
2019-06-12 12:04:36 -0400

    Updated flask routes and web files

--- app_ml.py
+++ app_ml.py
@@ -1,11 +1,21 @@
+# BIG TO DO: ADD ROUTES FOR INCOME IF WE DECIDE TO INCLUDE THAT
+# IF WE DO, NEED TO UPDATE BOTH JS AS WELL
+# IF WE DO NOT, ADJUST CLEAN SCRIPT APPROPRIATELY
+# IF WE DECIDE NOT TO INCLUDE CITIES, GET RID OF THAT CODE
+
+# Import dependencies
 import json
 import sqlite3
 import pandas as pd
 import os
 
-from flask import Flask, jsonify
-from flask import request
-from flask import make_response
+from flask import (
+    Flask,
+    render_template,
+    jsonify,
+    request,
+    make_response)
+
 from clean import (
     clean_school,
     clean_income_zip,
@@ -16,17 +26,43 @@ app = Flask(__name__)
 # Create database
 conn = sqlite3.connect('nj_db.db')
 
-# school_file = open('school.geojson')
-# school_json = json.load(school_file)
+# Add geojson data to database
+school_file = open(os.path.join("Resources", "school.geojson"))
+school_json = json.load(school_file)
+hospital_file = open(os.path.join("Resources", "hospitals.geojson"))
+hospital_json = json.load(hospital_file)
 
-# Clean data and enter into database
+# Clean school, income, zip code, and hospital data into database
 clean_school()
 clean_income_zip()
 clean_hospital()
 
 #Create routes 
-@app.route('/HOSPITAL/COUNTIES/<COUNTY>')
+
+@app.route("/")
+def index():
+    """Render the homepage."""
+    
+    # PUT CODE HERE
+    return render_template("index.html")
+
+
+@app.route('/school/counties/<COUNTY>')
+def school_county(COUNTY):
+    """DESCRIBE WHAT THIS DOES"""
+
+    conn = sqlite3.connect('nj_db.db')
+    c = conn.cursor()
+    data = c.execute('SELECT * FROM school WHERE COUNTY = ?',[COUNTY]).fetchall()
+    conn.commit()
+    conn.close()
+    return jsonify(data)
+
+
+@app.route('/hospital/counties/<COUNTY>')
 def hospital_county(COUNTY):
+    """DESCRIBE WHAT THIS DOES"""
+
     conn = sqlite3.connect('nj_db.db')
     c = conn.cursor()
     data = c.execute('SELECT * FROM hospital WHERE COUNTY = ?',[COUNTY]).fetchall()
@@ -34,33 +70,59 @@ def hospital_county(COUNTY):
     conn.close()
     return jsonify(data)
 
-@app.route('/SCHOOL/COUNTIES/<COUNTY>')
-def school_county(COUNTY):
+
+@app.route('/school/state')
+def school_state():
+    """DESCRIBE WHAT THIS DOES"""
+
     conn = sqlite3.connect('nj_db.db')
     c = conn.cursor()
-    data = c.execute('SELECT * FROM school WHERE COUNTY = ?',[COUNTY]).fetchall()
+    
+    # HERE'S WHERE THE WORK GOERS
+    # data = c.execute()
+    
     conn.commit()
     conn.close()
     return jsonify(data)
+
+
+@app.route('/hospital/state')
+def hospital_state():
+    """DESCRIBE WHAT THIS DOES"""
+
+    conn = sqlite3.connect('nj_db.db')
+    c = conn.cursor()
+    
+    # HERE'S WHERE THE WORK GOERS
+    # data = c.execute()
+    
+    conn.commit()
+    conn.close()
+    return jsonify(data)
+
+# REVISIT THIS LATER
+# @app.route('/school/cities/<CITY>')
+# def school_city(CITY):
+#     """DESCRIBE WHAT THIS DOES"""
+    
+#     conn = sqlite3.connect('nj_db.db')
+#     c = conn.cursor()
+#     data = c.execute('SELECT * FROM school WHERE CITY = ?',[CITY]).fetchall()
+#     conn.commit()
+#     conn.close()
+#     return jsonify(data)
    
-@app.route('/HOSPITAL/CITIES/<CITY>')
-def hospital_city(CITY):
-    conn = sqlite3.connect('nj_db.db')
-    c = conn.cursor()
-    data = c.execute('SELECT * FROM hospital WHERE CITY = ?',[CITY]).fetchall()
-    conn.commit()
-    conn.close()
-    return jsonify(data)
 
-@app.route('/SCHOOL/CITIES/<CITY>')
-def school_city(CITY):
-    conn = sqlite3.connect('nj_db.db')
-    c = conn.cursor()
-    data = c.execute('SELECT * FROM school WHERE CITY = ?',[CITY]).fetchall()
-    conn.commit()
-    conn.close()
-    return jsonify(data)
-
+# @app.route('/hospital/cities/<CITY>')
+# def hospital_city(CITY):
+#     """DESCRIBE WHAT THIS DOES"""
+    
+#     conn = sqlite3.connect('nj_db.db')
+#     c = conn.cursor()
+#     data = c.execute('SELECT * FROM hospital WHERE CITY = ?',[CITY]).fetchall()
+#     conn.commit()
+#     conn.close()
+#     return jsonify(data)
 
 if __name__ == '__main__':
     app.run(debug=False, port=8000, host='localhost', threaded=True)
--- static/js/visual.js
+++ static/js/visual.js
@@ -1,3 +1,196 @@
-// Define three main functions that create our visualizations
-// Additional functions can be defined that call those functions
+// Define four main functions that create our visualizations
 // These functions will be called inside of logic.js 
+
+function schoolCountyPlot(county) {
+
+    /**
+    /* Builds chart for school data for given county
+    /* @param {string}    county    Name of selected county
+    */
+  
+    // Construct url for path to school data for selected county
+    var url = `/school/counties/${county}`;
+  
+    // Fetch school data for the county
+    d3.json(url).then(function(schoolData){
+  
+      // Unpack and/or futz with data
+
+      // Create trace
+      var trace = {
+      };
+
+      // Create data
+      var data = [trace];
+      // Create layout
+      var layout = {
+      };
+      // Generate new plot
+      Plotly.newPlot("county-school-plot", data, layout);
+    });
+}
+
+function schoolNJPlot() {
+
+    /**
+    /* Builds chart for school data for all of NJ
+    */
+  
+    // Construct url for path to school data for state
+    var url = `/school/state`;
+  
+    // Fetch school data for state
+    d3.json(url).then(function(schoolData){
+  
+      // Unpack and/or futz with data
+
+      // Create trace
+      var trace = {
+      };
+
+      // Create data
+      var data = [trace];
+      // Create layout
+      var layout = {
+      };
+      // Generate new plot
+      Plotly.newPlot("state-school-plot", data, layout);
+    });
+}
+
+function hospitalCountyPlot(county) {
+
+    /**
+    /* Builds chart for hospital data for given county
+    /* @param {string}    county    Name of selected county
+    */
+  
+    // Construct url for path to hospital data for selected county
+    var url = `/hospital/counties/${county}`;
+  
+    // Fetch hospital data for the county
+    d3.json(url).then(function(hospitalData){
+  
+      // Unpack and/or futz with data
+
+      // Create trace
+      var trace = {
+      };
+
+      // Create data
+      var data = [trace];
+      // Create layout
+      var layout = {
+      };
+      // Generate new plot
+      Plotly.newPlot("county-hospital-plot", data, layout);
+    });
+}
+
+function hospitalNJPlot() {
+
+    /**
+    /* Builds chart for hospital data for all of NJ
+    */
+  
+    // Construct url for path to hospital data for state
+    var url = `/hospital/state`;
+  
+    // Fetch hospital data for the county
+    d3.json(url).then(function(hospitalData){
+  
+      // Unpack and/or futz with data
+
+      // Create trace
+      var trace = {
+      };
+
+      // Create data
+      var data = [trace];
+      // Create layout
+      var layout = {
+      };
+      // Generate new plot
+      Plotly.newPlot("state-hospital-plot", data, layout);
+    });
+}
+
+// TO DO: THIS IS VERY ROUGH
+
+function reportCard(county) {
+
+    /**
+    /* Builds report card for given county
+    /* @param {string}    county    Name of selected county
+    */
+   
+    // Construct url for path to school data for selected school
+   var url_school = `/school/counties/${county}`;
+  
+   // Clear existing report card
+   d3.select("#report-card").html("");
+
+   // Fetch school data for the county
+   d3.json(url_school).then(function(schoolData){
+ 
+     // Unpack and/or futz with data
+
+     // Do some sort of mathematical something to create numbers and grade
+     // Maybe find the median math and english SAT score
+     // If it falls within a certain range, it gets an A, etc.
+     // We could also find other metrics
+
+    //  var mathMed = math using stuff from schoolData;
+    //  var engMed = math using stuff from schoolData;
+    //  var schoolGrade = math using stuff from schoolData;
+
+     // Create the fields and fill in the data
+     // Something like:
+     // p is probably not right for append
+     // you can jam html directly into this OR change css file by class
+     d3.select("#report-card")
+        .append("p") 
+        .classed("", true)
+        .text(`Median Math SAT: ${mathMed}`)
+        .append("p")
+        .classed("", true)
+        .text(`Median English SAT: ${engMed}`)
+        .append("p")
+        .classed("", true)
+        .text(`School Grade: ${schoolGrade}`);
+   });
+   
+   // Construct url for path to hospital data for selected county
+   var url_hospital = `/hospital/counties/${county}`;
+  
+   // Fetch hospital data for the county
+   d3.json(url_hospital).then(function(hospitalData){
+ 
+     // Unpack and/or futz with data
+
+     // Do Math
+    //  var hospMed = math...;
+    //  var hospGrade = math..;
+
+     // Fill in fields for hospital in report card
+
+     d3.select("#report-card")
+        .append("p") 
+        .classed("", true)
+        .text(`Median Hospital Score: ${hospMed}`)
+        .append("p")
+        .classed("", true)
+        .text(`Hospital Grade: ${hospGrade}`);
+   });
+
+   // Do Math
+//    var countyGrade = math...;
+
+   d3.select("#report-card")
+    .append("p") 
+    .classed("", true)
+    .text(`County Grade: ${countyGrade}`)
+}
+
+
+
