e35bd12847042854154a9071d49a362ce44a6558
Gretel <gretelup@gmail.com>
2019-06-12 22:30:52 -0400

    Cleanup and leaflet stuff

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,3 +1,6 @@
+// ARJUN TO DO: NEED TO ADD PLACES TO CSS CODE SOMWHERE
+
+
 // Create school and hospital plots for NJ
 // schoolNJPlot();
 // hospitalNJPlot();
@@ -20,27 +23,37 @@
 //   });
 // });
 
-// TO DO: FIGURE OUT FORMATTING AND ZOOM OF MAP OBJECT
-// THIS NEEDS TO BE TAKEN CARE OF IN STYLE.CSS AND INDEX.HTML
+// GRETEL - MAKE SURE THERE IS CODE FOR UNSELECTING
+// Set style for layers
+var stylelayer = {
+  default: {
+    color: "blue",
+    opacity: 1,
+    fillcolor:"blue",
+    fillOpacity:0.1,
+    weight: 0.5
+  }
+  ,
+  reset:{
+    color: "blue",
+    opacity: 0.4,
+    weight: 1
+  }
+  ,
+  highlight:{
+    weight: 5,
+    color: "red",
+    dashArray: '',
+    fillOpacity: 0.7
+  }
+  ,
+  selected:{
+    color: "green",
+    opacity: 0.3,
+    weight: 0.5
+  }
 
-// Create map object
-var myMap = L.map("map", {
-    center: [40.0583, -74.4057],
-    zoom: 8
-  });
-
-
-// TO DO: FIGURE OUT WHICH TILE LAYER WE WANT
-// LOOK AT MAPBOX DOCUMENTATION FOR THIS
-
-// Add tile layer
-L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-  maxZoom: 18,
-  id: "mapbox.streets",
-  accessToken: API_KEY
-}).addTo(myMap);
-
+}
 // TO DO: ADD ADDITIONAL LAYERS???
 // INCOME LAYER - CHLOROPLETH??
 // HOSPITAL MARKERS
@@ -48,6 +61,24 @@ L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
 // ABILITY TO TURN OFF/ON
 // MAYBE WAIT TO ADD THE LAYERS?
 // Add income layer
+
+// TO DO: FIGURE OUT FORMATTING AND ZOOM OF MAP OBJECT
+// THIS NEEDS TO BE TAKEN CARE OF IN STYLE.CSS AND INDEX.HTML
+
+// Create map object and add layer
+var myMap = L.map("map", {
+    center: [40.0583, -74.4057],
+    zoom: 8
+  });
+
+L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  maxZoom: 18,
+  id: "mapbox.streets",
+  accessToken: API_KEY
+}).addTo(myMap);
+
+
 
 // TO DO: NEED TO CONNECT INFORMATION FROM COUNTY GEOJSON
 // SO WHEN WE CLICK ON A COUNTY, ALL THE STUFF WILL HAPPEN
@@ -61,8 +92,42 @@ L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
 // Add counties
 var link = "https://opendata.arcgis.com/datasets/5f45e1ece6e14ef5866974a7b57d3b95_1.geojson";
 
+// DO ALL THE STUFF HERE! - FOR THE POPUP, just the name of the county
+// ALSO NEED TO ADD THE OTHER STUFF THAT HAPPENS WITH THE CLICK EVENT
+// https://github.com/olanaso/Leaflet-Select-Polygons/blob/master/Leaflet-Select-Polygons.js
+
+// Declare global variables
+var countyNames = [];
+var countyDisplayName = {};
+
+// Get geojson file for NJ counties
+
+// IS THIS THE CORRECT SYNTAX???
+var countyData = d3.json(link);
+
+var countyGJson = L.geoJson(countyData, {
+  style: stylelayer.default,
+  onEachFeature: onEachFeature
+}).addTo(map);
+
+
 d3.json(link, function(data) {
   // Creating a GeoJSON layer with the retrieved data
   L.geoJson(data).addTo(myMap)
+
+  // Create a list of counties and associated display name
+  $.each(data.features, function(index,feature){
+		var county = feature.properties.COUNTY
+		countyNames.push(county);
+		countyDisplayName[countyName] = feature.properties.GNIS_Name;
+  });
+
+
+  
+
+
+   // GET VALUE FOR COUNTY AND ATTACH IT TO MAP AREA
+  // COUNTY IS "FEATURES" --> "properties" --> "COUNTY" (e.g. ATLANTIC)
+  // County Label is "FEATURES" --> "properties" --> "GNIS_Name" (e.g. Atlantic County)
 
 });
