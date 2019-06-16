cfddbf7bc1e5a668f4f2c46cd416deec4078759b
Gretel <gretelup@gmail.com>
2019-06-15 23:00:26 -0400

    Updated bar chart

--- static/js/visual.js
+++ static/js/visual.js
@@ -1,6 +1,3 @@
-// Define four main functions that create our visualizations
-// These functions will be called inside of logic.js 
-
 function schoolCountyPlot(county) {
 
   /**
@@ -13,101 +10,192 @@ function schoolCountyPlot(county) {
 
   // Fetch school data for the county
   d3.json(url).then(function (schoolData) {
-    
-    // unpack jSON
+
+    // Unpack data
     var math_avg = schoolData[0].math_avg;
     var eng_avg = schoolData[0].eng_avg;
     var math_state_avg = schoolData[0].math_state_avg;
     var eng_state_avg = schoolData[0].eng_state_avg;
 
-    //Group bar chart for SAT scores
-    // Create trace
+    // Create plot parameterse for bar chart
     var trace1 = {
       x: ["Math SAT", "English SAT"],
       y: [math_avg, eng_avg],
       name: `${county} County`,
       type: 'bar'
     };
-
     var trace2 = {
       x: ["Math SAT", "English SAT"],
       y: [math_state_avg, eng_state_avg],
       name: 'State',
       type: 'bar'
     };
-    
     var data = [trace1, trace2];
-  
+    var layout = { barmode: 'group' };
 
-  var layout = { barmode: 'group' };
-
-  // Generate new plot
-  Plotly.newPlot("plot1", data, layout);
-});
+    // Generate bar chart
+    Plotly.newPlot("plot1", data, layout);
+  });
 }
 
-function schoolNJPlot() {
+
+function schoolNJPlotInit() {
 
   /**
-  /* Builds chart for school data for all of NJ
+  /* Builds initial chart for school data for whole state
   */
+
+  // Define SVG area dimensions
+  var svgWidth = 600;
+  var svgHeight = 400;
+
+  // Define margins and dimensions of chart area
+  var chartMargin = {
+    top: 30,
+    right: 30,
+    bottom: 70,
+    left: 40
+  };
+  var chartWidth = svgWidth - chartMargin.left - chartMargin.right;
+  var chartHeight = svgHeight - chartMargin.top - chartMargin.bottom;
+
+  // Create svg object and append a group
+  var svg = d3.select("#plot2")
+    .append("svg")
+    .attr("width", svgWidth)
+    .attr("height", svgHeight);
+  var chartGroup = svg.append("g")
+    .attr("transform", `translate(${chartMargin.left}, ${chartMargin.top})`);
+
+  // Creat a tolltip object
+  var toolTip = d3.tip()
+    .attr("class", "d3-tip")
+    .offset([-10, 0])
+    .html(function (d) {
+      return (`<strong>${d.county}</strong><br>${d.sat_avg}`);
+    })
 
   // Construct url for path to school data for state
   var url = `/school/state`;
 
-  // Fetch school data for state
+  // Fetch school data for all counties in the state
   d3.json(url).then(function (schoolData) {
 
-      // Cast data as integers
-      schoolData.forEach(function (d) {
-          d.sat_avg = +d.sat_avg;
-      });
+    // Round SAT averages
+    schoolData.forEach(function (d) {
+      d.sat_avg = Math.round(+d.sat_avg);
+    });
 
-      $('#plot2').animatedBarChart({
-          data: schoolData,
-          params: {
-              name: 'County', // name for xaxis
-              value: 'Average SAT Score' // value for yaxis
-          },
-          chart_height: 400,
-          horizontal_bars: false,
-          colors: null,
-          show_legend: false,
-          x_grid_lines: true,
-          y_grid_lines: true,
-          // speed for tranistions
-          tweenDuration: 300,
-          bars: {
-              padding: 0.075, // padding between bars
-              opacity: 0.7, // default bar opacity
-              opacity_hover: 0.45, // default bar opacity on mouse hover
-              disable_hover: false, // disable animation and legend on hover
-              hover_name_text: 'county', // text for name column for label displayed on bar hover
-              hover_value_text: 'sat_avg', // text for value column for label displayed on bar hover
-          },
-          // margins for chart rendering
-          margin: {
-              top: 0, // top margin
-              right: 35, // right margin
-              bottom: 20, // bottom margin
-              left: 70 // left margin
-          },
-          // rotate xaxis label params
-          rotate_x_axis_labels: {
-              process: true, // process xaxis label rotation
-              minimun_resolution: 720, // minimun_resolution for label rotating
-              bottom_margin: 15, // bottom margin for label rotation
-              rotating_angle: 90, // angle for rotation,
-              x_position: 9, // label x position after rotation
-              y_position: -3 // label y position after rotation
-          }
+    // Configure x and y scales
+    var xBandScale = d3.scaleBand()
+      .domain(schoolData.map(d => d.county))
+      .range([0, chartWidth])
+      .padding(0.2);
+    var yLinearScale = d3.scaleLinear()
+      .domain([0, 1600])
+      .range([chartHeight, 0]);
+
+    // Define functions to create axes
+    var bottomAxis = d3.axisBottom(xBandScale);
+    var leftAxis = d3.axisLeft(yLinearScale).ticks(10);
+
+    // Append axes to chart
+    chartGroup.append("g")
+      .call(leftAxis);
+    chartGroup.append("g")
+      .attr("transform", `translate(0, ${chartHeight})`)
+      .call(bottomAxis)
+      .selectAll("text")
+      .style("text-anchor", "end")
+      .attr("dx", "-.8em")
+      .attr("dy", ".15em")
+      .attr("transform", "rotate(-65)");
+
+    //Create bars for chart
+    chartGroup.selectAll("rect")
+      .data(schoolData)
+      .enter()
+      .append("rect")
+      .attr("width", xBandScale.bandwidth())
+      .attr("height", d => chartHeight - yLinearScale(d.sat_avg))
+      .attr("x", d => xBandScale(d.county))
+      .attr("y", d => yLinearScale(d.sat_avg))
+      .attr("fill", "blue")
+      .on("mouseover", toolTip.show)
+      .on("mouseout", toolTip.hide)
+
+    // Add tooltips for chart
+    chartGroup.call(toolTip);
+  });
+}
+
+
+function schoolNJPlot(county) {
+
+  /**
+  /* Updates state school bar chart for selected given county
+  /* @param {string}    county    Name of selected county
+  */
+
+  // Create svg object
+  var svg = d3.select("#plot2")
+
+  // Construct url for path to school data for state
+  var url = `/school/state`;
+
+  // Fetch school data for all counties in the state
+  d3.json(url).then(function (schoolData) {
+
+    // Create object for all bars in chart
+    var bars = svg.selectAll("rect")
+      .data(schoolData)
+
+    // Update color of bar for selected copy
+    bars.enter()
+      .append("rect")
+      .merge(bars)
+      .transition()
+      .duration(100)
+      .attr("fill", function (d) {
+        if (d.county == county) {
+          return ("red");
+        }
+        else {
+          return ("blue");
+        }
       });
   });
 }
 
-// TO TEST:
+function schoolNJPlotReset() {
 
-// schoolNJPlot();
+  /**
+  /* Resets colors of state school bar chart
+  */
+
+  // Create svg object
+  var svg = d3.select("#plot2")
+
+  // Construct url for path to school data for state
+  var url = `/school/state`;
+
+  // Fetch school data for all counties in the state
+  d3.json(url).then(function (schoolData) {
+
+    // Create object for all bars in chart
+    var bars = svg.selectAll("rect")
+      .data(schoolData)
+
+    // Update color of bar for selected copy
+    bars.enter()
+      .append("rect")
+      .merge(bars)
+      .transition()
+      .duration(100)
+      .attr("fill", "blue");
+  });
+}
+
 
 function hospitalCountyPlot(county) {
 
@@ -118,91 +206,88 @@ function hospitalCountyPlot(county) {
 
   // Construct url for path to hospital data for selected county
   var url = `/hospital/counties/${county}`;
-  rate_total=0
-  counter=0
+  
+  // Initialize counters variables
+  var rate_total = 0;
+  var counter = 0;
+
   // Fetch hospital data for the county
   d3.json(url).then(function (hospitalData) {
-    hospitalData.forEach(function(d) {
+    
+    // Calculate average hospital rate for county
+    hospitalData.forEach(function (d) {
       h_rate = +d.rate;
-      counter=counter+1;
-      rate_total=rate_total+h_rate
+      counter += 1;
+      rate_total += h_rate
     });
-  avg_h_rate=rate_total/counter;
-  // Convert avg rate to ratio to be equivalent to degrees for gauge 
-  console.log(avg_h_rate);
-  var level=avg_h_rate * 180 / 5;
-  // Trig to calculate meter point
-  var degrees = 180 - level,
-  radius = .5;
-  var radians = degrees * Math.PI / 180;
-  var x = radius * Math.cos(radians);
-  var y = radius * Math.sin(radians);        // Create gauge arrow
-  var mainPath = 'M -.0 -0.025 L .0 0.025 L ',
+    avg_h_rate = rate_total / counter;
+    
+    // Convert rate to ratio to be equivalent to degrees for gauge 
+    var level = avg_h_rate * 180 / 5;
+
+    // Trig to calculate meter point
+    var degrees = 180 - level,
+      radius = .5;
+    var radians = degrees * Math.PI / 180;
+    var x = radius * Math.cos(radians);
+    var y = radius * Math.sin(radians);        // Create gauge arrow
+    var mainPath = 'M -.0 -0.025 L .0 0.025 L ',
       pathX = String(x),
       space = ' ',
       pathY = String(y),
       pathEnd = ' Z';
-  var path = mainPath.concat(pathX,space,pathY,pathEnd);
+    var path = mainPath.concat(pathX, space, pathY, pathEnd);
 
-  // Construct gauge
-  var data = [{ type: 'scatter',
-  x: [0], y:[0],
-      marker: {size: 28, color:'850000'},
+    // Construct gauge
+    var data = [{
+      type: 'scatter',
+      x: [0], y: [0],
+      marker: { size: 28, color: '850000' },
       showlegend: false,
       name: 'ratings',
       text: avg_h_rate,
-      hoverinfo: avg_h_rate},
-  { values: [1, 1, 1, 1, 1,5],
-  rotation: 90,
-  text: ['5', '4', '3', '2', '1',"  "],
-  textinfo: 'text',
-  textposition:'inside',
-  marker: {colors:['#1db231','#5c3bc2',
-                   '#51BCF7','#ffba00', 
-                  '#e0455e','rgba(255, 255, 255, 0)']},
-  labels: ['Wicked Awesome', 'Pretty Good', 'Ok', 'Meh', 'Sucks',"  "],
-  hoverinfo: 'label',
-  hole: .5,
-  type: 'pie',
-  showlegend: false
-  }];
+      hoverinfo: avg_h_rate
+    },
+    {
+      values: [1, 1, 1, 1, 1, 5],
+      rotation: 90,
+      text: ['5', '4', '3', '2', '1', "  "],
+      textinfo: 'text',
+      textposition: 'inside',
+      marker: {
+        colors: ['#1db231', '#5c3bc2',
+          '#51BCF7', '#ffba00',
+          '#e0455e', 'rgba(255, 255, 255, 0)']
+      },
+      labels: ['Wicked Awesome', 'Pretty Good', 'Ok', 'Meh', 'Sucks', "  "],
+      hoverinfo: 'label',
+      hole: .5,
+      type: 'pie',
+      showlegend: false
+    }];
 
-  var layout = {
-  shapes:[{
-      type: 'path',
-      path: path,
-      fillcolor: '850000',
-      line: {
+    var layout = {
+      shapes: [{
+        type: 'path',
+        path: path,
+        fillcolor: '850000',
+        line: {
           color: '850000'
-      }
+        }
       }],
-  title: '<b>Based on county</b>',
-  height: 400,
-  width: 400,
-  xaxis: {zeroline:false, showticklabels:false,
-              showgrid: false, range: [-1, 1]},
-  yaxis: {zeroline:false, showticklabels:false,
-              showgrid: false, range: [-1, 1]}
-  };
-  Plotly.newPlot("plot3", data, layout);
-
-    // Unpack and/or futz with data
-
-    // Create trace
-    // var HospitalCountydata = [{
-    //   values: [],
-    //   labels: [, , ],
-    //   type: 'pie'
-    // }];
-    
-    // // Create layout
-    // var layout = {
-    //   height: x,
-    //   width: y
-    // };
-    
-    // // Generate new plot
-    // Plotly.newPlot("plot3", data, layout);
+      title: '<b>Based on county</b>',
+      height: 400,
+      width: 400,
+      xaxis: {
+        zeroline: false, showticklabels: false,
+        showgrid: false, range: [-1, 1]
+      },
+      yaxis: {
+        zeroline: false, showticklabels: false,
+        showgrid: false, range: [-1, 1]
+      }
+    };
+    Plotly.newPlot("plot3", data, layout);
   });
 }
 
@@ -216,88 +301,90 @@ function hospitalNJPlot() {
   // Construct url for path to hospital data for state
   var url = `/hospital/state`;
 
-  // Fetch hospital data for the county
+  // Initialize counters variables
+  var rate_total = 0;
+  var counter = 0;
+
+  // Fetch hospital data for the NJ
   d3.json(url).then(function (hospitalData) {
-    hospitalData.forEach(function(d) {
+    
+    // Calculate average hospital rate for state
+    hospitalData.forEach(function (d) {
       h_rate = +d.avg_rate;
-      counter=counter+1;
-      rate_total=rate_total+h_rate
+      counter = counter + 1;
+      rate_total = rate_total + h_rate
     });
-  avg_h_rate=rate_total/counter;
-  // Convert avg rate to ratio to be equivalent to degrees for gauge 
-  console.log(avg_h_rate);
-  var level=avg_h_rate * 180 / 5;
-  // Trig to calculate meter point
-  var degrees = 180 - level,
-  radius = .5;
-  var radians = degrees * Math.PI / 180;
-  var x = radius * Math.cos(radians);
-  var y = radius * Math.sin(radians);        // Create gauge arrow
-  var mainPath = 'M -.0 -0.025 L .0 0.025 L ',
+    avg_h_rate = rate_total / counter;
+    
+    // Convert avg rate to ratio to be equivalent to degrees for gauge 
+    var level = avg_h_rate * 180 / 5;
+    
+    // Trig to calculate meter point
+    var degrees = 180 - level,
+      radius = .5;
+    var radians = degrees * Math.PI / 180;
+    var x = radius * Math.cos(radians);
+    var y = radius * Math.sin(radians);        // Create gauge arrow
+    var mainPath = 'M -.0 -0.025 L .0 0.025 L ',
       pathX = String(x),
       space = ' ',
       pathY = String(y),
       pathEnd = ' Z';
-  var path = mainPath.concat(pathX,space,pathY,pathEnd);
+    var path = mainPath.concat(pathX, space, pathY, pathEnd);
 
-  // Construct gauge
-  var data = [{ type: 'scatter',
-  x: [0], y:[0],
-      marker: {size: 28, color:'850000'},
+    // Construct gauge
+    var data = [{
+      type: 'scatter',
+      x: [0], y: [0],
+      marker: { size: 28, color: '850000' },
       showlegend: false,
       name: 'ratings',
       text: avg_h_rate,
-      hoverinfo: avg_h_rate},
-  { values: [1, 1, 1, 1, 1,5],
-  rotation: 90,
-  text: ['5', '4', '3', '2', '1',"  "],
-  textinfo: 'text',
-  textposition:'inside',
-  marker: {colors:['#1db231','#5c3bc2',
-                   '#51BCF7','#ffba00', 
-                  '#e0455e','rgba(255, 255, 255, 0)']},
-  labels: ['Wicked Awesome', 'Pretty Good', 'Ok', 'Meh', 'Sucks',"  "],
-  hoverinfo: 'label',
-  hole: .5,
-  type: 'pie',
-  showlegend: false
-  }];
+      hoverinfo: avg_h_rate
+    },
+    {
+      values: [1, 1, 1, 1, 1, 5],
+      rotation: 90,
+      text: ['5', '4', '3', '2', '1', "  "],
+      textinfo: 'text',
+      textposition: 'inside',
+      marker: {
+        colors: ['#1db231', '#5c3bc2',
+          '#51BCF7', '#ffba00',
+          '#e0455e', 'rgba(255, 255, 255, 0)']
+      },
+      labels: ['Wicked Awesome', 'Pretty Good', 'Ok', 'Meh', 'Sucks', "  "],
+      hoverinfo: 'label',
+      hole: .5,
+      type: 'pie',
+      showlegend: false
+    }];
 
-  var layout = {
-  shapes:[{
-      type: 'path',
-      path: path,
-      fillcolor: '850000',
-      line: {
+    var layout = {
+      shapes: [{
+        type: 'path',
+        path: path,
+        fillcolor: '850000',
+        line: {
           color: '850000'
-      }
+        }
       }],
-  title: '<b>Based on state</b>',
-  height: 400,
-  width: 400,
-  xaxis: {zeroline:false, showticklabels:false,
-              showgrid: false, range: [-1, 1]},
-  yaxis: {zeroline:false, showticklabels:false,
-              showgrid: false, range: [-1, 1]}
-  };
-  Plotly.newPlot("plot4", data, layout);
-    // Unpack and/or futz with data
-
-    // // Create trace
-    // var trace = {
-    // };
-
-    // // Create data
-    // var data = [trace];
-    // // Create layout
-    // var layout = {
-    // };
-    // // Generate new plot
-    // Plotly.newPlot("plot4", data, layout);
+      title: '<b>Based on state</b>',
+      height: 400,
+      width: 400,
+      xaxis: {
+        zeroline: false, showticklabels: false,
+        showgrid: false, range: [-1, 1]
+      },
+      yaxis: {
+        zeroline: false, showticklabels: false,
+        showgrid: false, range: [-1, 1]
+      }
+    };
+    Plotly.newPlot("plot4", data, layout);
   });
 }
 
-// TO DO: THIS IS VERY ROUGH
 
 function reportCard(county) {
 
